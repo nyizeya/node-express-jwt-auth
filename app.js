f0bf4752364b62b603209883c43ea622681c5db8
nyizeya <nyizeya103@gmail.com>
2021-06-14 11:50:12 +0630

    lesson-1 added

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config')
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = process.env.DB_CONNECT;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => {
+    app.listen(3000)
+    console.log('server running on port 3000');
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
