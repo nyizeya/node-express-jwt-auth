f496f492a44a7c0b1e0be37cdcb6bb86ad6f495f
nyizeya <nyizeya103@gmail.com>
2021-06-16 11:22:27 +0630

    added lesson-8 with cookies staffs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
 require('dotenv/config')
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json())
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs');
@@ -26,3 +28,17 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true')
+  res.cookie('newUser', false)
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true})
+  res.send('you got the cookie')
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies
+  console.log(cookies);
+  res.json(cookies)
+})
